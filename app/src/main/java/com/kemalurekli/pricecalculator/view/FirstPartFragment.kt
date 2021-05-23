78d2a8f1361eb9e52a345e0c55fef649625e5ec8
kemalurekli <kemalurekli@gmail.com>
2021-05-23 21:46:54 +0300

    Firstpart's spinners added.

--- app/src/main/java/com/kemalurekli/pricecalculator/view/FirstPartFragment.kt
+++ app/src/main/java/com/kemalurekli/pricecalculator/view/FirstPartFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.kemalurekli.pricecalculator.R
 import com.kemalurekli.pricecalculator.databinding.FragmentFirstPartBinding
 import com.kemalurekli.pricecalculator.viewmodel.FirstPartFragmentViewModel
 
 
-class FirstPartFragment : Fragment() {
+class FirstPartFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private var _binding: FragmentFirstPartBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel : FirstPartFragmentViewModel
+    private var select1 : Int? = null
 
 
     override fun onCreateView(
@@ -33,12 +38,23 @@ class FirstPartFragment : Fragment() {
         viewModel = ViewModelProvider(this)[FirstPartFragmentViewModel::class.java]
         viewModel.getParameters()
         observeLiveData()
+        //For Spinner
+        val spinner: Spinner = binding.firstSpinner
+        val spinner2 : Spinner = binding.secondSpinner
+        spinner.onItemSelectedListener = this
+        spinner2.onItemSelectedListener = this
 
-
+        ArrayAdapter.createFromResource(requireContext(), R.array.planets_array, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        ArrayAdapter.createFromResource(requireContext(), R.array.months_array, android.R.layout.simple_spinner_item).also { adapter2 ->
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner2.adapter = adapter2
+        }
 
 
     }
-
     private fun observeLiveData() {
 
         viewModel.getParameters.observe(viewLifecycleOwner, Observer {getParameters ->
@@ -48,8 +64,17 @@ class FirstPartFragment : Fragment() {
         })
     }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        select1 = position
+        println(select1)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
--- app/src/main/java/com/kemalurekli/pricecalculator/viewmodel/FirstPartFragmentViewModel.kt
+++ app/src/main/java/com/kemalurekli/pricecalculator/viewmodel/FirstPartFragmentViewModel.kt
@@ -1,8 +1,11 @@
 package com.kemalurekli.pricecalculator.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.kemalurekli.pricecalculator.model.ParameterData
+import com.kemalurekli.pricecalculator.view.FirstPartFragment
+
 
 class FirstPartFragmentViewModel : ViewModel() {
     val getParameters = MutableLiveData<List<ParameterData>>()
@@ -14,6 +17,10 @@ class FirstPartFragmentViewModel : ViewModel() {
         val parameterList = arrayListOf<ParameterData>(parameter1)
 
         getParameters.value = parameterList
+
     }
+    
+
+
 
 }
